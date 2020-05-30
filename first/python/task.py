e5c45482564471cfca55968bab50650b952c79f9
dnchk <no-reply>
2020-05-30 11:19:09 +0300

    Add help param

--- first/python/task.py
+++ first/python/task.py
@@ -1,7 +1,8 @@
-import pathlib
 import os
+import sys
+import pathlib
 
-def getCurrentDir():
+def gerCurrentDir():
     return os.getcwd()
 
 def createDir(path):
@@ -16,12 +17,32 @@ def createDir(path):
 def moveFile(curr, dest):
     os.rename(curr, dest)
 
-curr_dir = getCurrentDir()
+usage = "Usage: task.py SOURCE_DIR"
 
-for root, dirs, files in os.walk(curr_dir):
+if len(sys.argv) == 1:
+    print(usage)
+    exit(-1)
+
+if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    print(usage)
+    exit(0)
+
+source_dir = sys.argv[1]
+
+if not os.path.isdir(source_dir):
+    print("Param is not a dir, see --help or -h")
+    exit(-1)
+
+for root, dirs, files in os.walk(source_dir):
     for file in files:
         filename, file_extension = os.path.splitext(file)
+
         if file_extension != ".swp" and file_extension != ".py":
-            if createDir(file_extension[1:]) == 0:
-                path_new = os.path.join(root, file_extension[1:])
-                moveFile(os.path.join(root, file), os.path.join(path_new, file))
+            if createDir(source_dir + "/" + file_extension[1:]) == -1:
+                continue
+
+            curr_dir = gerCurrentDir()
+            path_new = os.path.join(root, file_extension[1:])
+            whole_path_old = curr_dir + "/" + os.path.join(root, file)
+            whole_path_new = curr_dir + "/" + path_new + "/" + file
+            moveFile(whole_path_old, whole_path_new)
