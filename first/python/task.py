234bf8f747c60c01414bc38cbbe41605611d7c19
dnchk <no-reply>
2020-05-30 14:25:29 +0300

    Refactor dir creation

--- first/python/task.py
+++ first/python/task.py
@@ -9,10 +9,8 @@ def createDir(path):
     if not os.path.exists(path):
         os.mkdir(path)
         print("Directory", path, "created")
-        return 0
     else:
         print("Directory", path, "already exists")
-        return -1
 
 def moveFile(curr, dest):
     os.rename(curr, dest)
@@ -33,16 +31,16 @@ if not os.path.isdir(source_dir):
     print("Param is not a dir, see --help or -h")
     exit(-1)
 
-for root, dirs, files in os.walk(source_dir):
-    for file in files:
-        filename, file_extension = os.path.splitext(file)
+curr_path = gerCurrentDir()
 
-        if file_extension != ".swp" and file_extension != ".py":
-            if createDir(source_dir + "/" + file_extension[1:]) == -1:
-                continue
+for file in os.listdir(source_dir):
+    filename, file_extension = os.path.splitext(file)
+    if not file_extension:
+        continue
 
-            curr_dir = gerCurrentDir()
-            path_new = os.path.join(root, file_extension[1:])
-            whole_path_old = curr_dir + "/" + os.path.join(root, file)
-            whole_path_new = curr_dir + "/" + path_new + "/" + file
-            moveFile(whole_path_old, whole_path_new)
+    old_path = os.path.join(curr_path, source_dir, file)
+    new_dir = os.path.join(source_dir, file_extension[1:])
+    new_path = os.path.join(curr_path, new_dir, file)
+
+    createDir(new_dir)
+    moveFile(old_path, new_path)
