ea6c69d328534655e0146445c3fef9227f14bb00
dnchk <no-reply>
2020-06-07 00:15:28 +0300

    Accommodate python implementation to new test

--- first/python/task.py
+++ first/python/task.py
@@ -1,28 +1,25 @@
+#!/usr/bin/env python
+
 import os
 import sys
-import pathlib
-
-def gerCurrentDir():
-    return os.getcwd()
 
 def createDir(path):
     if not os.path.exists(path):
         os.mkdir(path)
-        print("Directory", path, "created")
-    else:
-        print("Directory", path, "already exists")
 
 def moveFile(curr, dest):
     os.rename(curr, dest)
 
-usage = "Usage: task.py SOURCE_DIR"
+def print_help():
+    print("Usage: task.py SOURCE_DIR")
+    print("Sort all the files in a SOURCE_DIR into subdirectories named by file extension")
 
 if len(sys.argv) != 2:
-    print(usage)
+    print_help()
     exit(-1)
 
 if sys.argv[1] == "-h" or sys.argv[1] == "--help":
-    print(usage)
+    print_help()
     exit(0)
 
 source_dir = sys.argv[1]
@@ -31,16 +28,14 @@ if not os.path.isdir(source_dir):
     print("Param is not a dir, see --help or -h")
     exit(-1)
 
-curr_path = gerCurrentDir()
-
 for file in os.listdir(source_dir):
     filename, file_extension = os.path.splitext(file)
     if not file_extension:
         continue
 
-    old_path = os.path.join(curr_path, source_dir, file)
+    old_path = os.path.join(source_dir, file)
     new_dir = os.path.join(source_dir, file_extension[1:])
-    new_path = os.path.join(curr_path, new_dir, file)
+    new_path = os.path.join(new_dir, file)
 
     createDir(new_dir)
     moveFile(old_path, new_path)
