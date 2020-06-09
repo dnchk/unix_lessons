9a596fac71d20a4e21e20b76ae6e13c7650cf63b
dnchk <no-reply>
2020-06-09 15:56:03 +0300

    Fix error case

--- first/perl/task.pl
+++ first/perl/task.pl
@@ -5,7 +5,7 @@ use File::Copy;
 use File::Basename;
 
 sub createDir {
-    if (not -e $_[0] and not -d $_[0]) {
+    if (not -e $_[0]) {
 	unless(mkdir $_[0]) {
 	    print "Unable to create $_[0]\n";
 	    return;
@@ -18,28 +18,28 @@ sub moveFile {
 }
 
 sub print_help {
-    print "Usage: task.pl SOURCE_DIR\n";
-    print "Sort all the files in a SOURCE_DIR into subdirectories named by file extension\n";
+    say STDOUT "Usage: task.pl SOURCE_DIR";
+    say STDOUT "Sort all the files in a SOURCE_DIR into subdirectories named by file extension";
 }
 
 if ($#ARGV != 0) {
-    print_help();
-    exit 0;
+    say STDERR "Usage: task.pl SOURCE_DIR, see -h or --help";
+    exit 1;
 }
 
 if ($ARGV[0] eq "-h" or $ARGV[0] eq "--help") {
     print_help();
-    exit 1;
+    exit 0;
 }
 
 $source_dir = $ARGV[0];
 
 if (not -d $source_dir) {
-    print "Param is not a dir, see --help or -h\n";
-    exit 0;
+    say STDERR "Param is not a dir, see --help or -h";
+    exit 1;
 }
 
-opendir (DIR, $source_dir) || print "Cannot open dir: $!";
+opendir (DIR, $source_dir) || say STDERR "Cannot open dir: $!";
 while(($file = readdir(DIR))) {
     my ($extension) = $file =~ /(\.[^.]+)$/;
     if ($extension ne "") {
